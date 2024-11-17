d6234c8e5c1999ac72af3c5718d0c57f99e6e661
一德 <1028647581@qq.com>
2024-11-17 13:18:02 +0800

    add:新增index.html页面，方便快速获得云订阅链接；日历新增时间戳 change:删除没必要的代码片段；更改了换行方式为\n。

--- timeSwitch.py
+++ timeSwitch.py
@@ -1,6 +1,4 @@
-from datetime import datetime, time
-import pytz
-
+from datetime import datetime
 
 def convert_time_start(date_str):
     # 解析字符串 "2025.01.10" 为 datetime 对象
@@ -27,30 +25,6 @@ def convert_time_end(date_str):
     new_date_str = new_date_obj.strftime("%Y%m%d")
     
     return new_date_str
-
-
-def is_after_four_am():
-    """
-    判断当前时间是否已经到达或超过 4:00 AM。
-    返回 True 如果当前时间 >= 4:00 AM，否则返回 False。
-    """
-    # 获取中国时区
-    china_timezone = pytz.timezone('Asia/Shanghai')
-    
-    # 获取当前时间
-    now = datetime.now(china_timezone)
-    
-    # 获取当天的4:00 AM时间，并转换为 Unix 时间戳
-    today = now.date()  # 当前日期
-    four_am_time = datetime.combine(today, time(4, 0), tzinfo=china_timezone)  # 4:00 AM
-    four_am_timestamp = int(four_am_time.timestamp())  # 4:00 AM的Unix时间戳
-    
-    # 获取当前时间的 Unix 时间戳（只保留小时和分钟）
-    current_time = now.replace(second=0, microsecond=0)  # 当前时间，秒和微秒为0
-    current_timestamp = int(current_time.timestamp())  # 当前时间的Unix时间戳
-    
-    # 判断当前时间是否已到 4:00 AM
-    return current_timestamp >= four_am_timestamp
 
 
 def get_today_date():
